990a4e541a670cb7dd1daba7b0131813b8389cdd
Nathan Serna <nath2534@outlook.com>
2024-10-06 18:50:09 -0500

    finished MainPagination.vue

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -1,11 +1,11 @@
 <template>
 
  <div class="flex justify-center gap-4 p-8">
-        <button @click="prev" :disabled="activePage === 1" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md">PREV</button>
+        <button @click="prev" :disabled="activePage === 1" class="rounded-md bg-emerald-400 p-2 font-medium text-slate-700 shadow-md">PREV</button>
 
-        <button v-for="page in pages" :key="page" class= "rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md" :class="page ===activePage ? 'bg-yellow-700 text-slate-100' : '' ">{{ page }}</button>
+        <button v-for="page in pages" :key="page" class= "rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md" :class="page ===activePage ? 'bg-red-600 text-slate-100' : '' ">{{ page }}</button>
 
-        <button @click ="next" :disable="activePage ===pages" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md">NEXT</button>
+        <button @click ="next" :disable="activePage ===pages" class="rounded-md bg-emerald-400 p-2 font-medium text-slate-700 shadow-md">NEXT</button>
     </div>
 
 </template>
@@ -25,7 +25,7 @@
 
         import { ref } from 'vue'
 
-        const pages = ref(10)
+        const pages = ref(20)
         const activePage = ref(2)
 
         const prev = () => {
