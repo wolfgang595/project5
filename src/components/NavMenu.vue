c946168bfdcae4eeafa1086bf5696c3751484482
Nathan Serna <nath2534@outlook.com>
2024-10-06 06:20:25 -0500

    updated nav bar

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -2,8 +2,29 @@
   <nav class="bg-slate-900 h-20 items-center text-white flex justify-center space-x-4">
     <RouterLink :to="{name: 'Home'}">Home</RouterLink>
     <RouterLink :to="{name: 'Other'}">Other</RouterLink>
-    <RouterLink :to="{name: 'LoginPage'}">Login</RouterLink>
+    
     <RouterLink :to="{name: 'SettingsPage'}">Settings</RouterLink>
+
+
+
+
+
+    <div v-if="isAuthenticated">
+      Welcome {{ user.email }}!
+      <button @click="logout" class="rounded-md bg-red-500 p-4 mx-2 text-red-100 hover:bg-red-700">LOGOUT</button>
+    </div>
+
+  <div v-else>
+  <RouterLink :to="{name: 'LoginPage'}">Login</RouterLink>
+  </div>
+
+
+
+
+
+
+
+
   </nav>
 </template>
 
@@ -16,3 +37,12 @@ nav {
   }
 }
 </style> -->
+
+
+<script setup>
+
+import { useAuth } from '../composables/useAuth'
+const { isAuthenticated, logout, user } = useAuth()
+
+
+</script>
