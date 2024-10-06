855bbfc4277878605607bad80f54c2a7f299e42c
Nathan Serna <nath2534@outlook.com>
2024-10-06 06:33:19 -0500

    Pagination done and added stlye

---
+++ src/components/MainPagination.vue
@@ -0,0 +1,47 @@
+<template>
+
+ <div class="flex justify-center gap-4 p-8">
+        <button @click="prev" :disabled="activePage === 1" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md">PREV</button>
+
+        <button v-for="page in pages" :key="page" class= "rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md" :class="page ===activePage ? 'bg-blue-400' : 'bg-purple-400' ">{{ page }}</button>
+
+        <button @click ="next" :disable="activePage ===pages" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md">NEXT</button>
+    </div>
+
+</template>
+
+
+
+
+
+
+
+
+
+
+<script setup>
+
+
+
+        import { ref } from 'vue'
+
+        const pages = ref(10)
+        const activePage = ref(5)
+
+        const prev = () => {
+            if (activePage.value > 1) {
+                activePage.value--
+                console.log(activePage.value)
+            }
+        }
+
+        const next = () => {
+            if (activePage.value < pages.value) {
+                activePage.value++
+                console.log(activePage.value)
+            }
+        }
+
+
+
+</script>
