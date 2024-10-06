7e3dc19719e37bef67171069bb094dce05fffc09
Nathan Serna <nath2534@outlook.com>
2024-10-06 04:30:31 -0500

    fixed rounded corners on image and card

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -18,30 +18,15 @@ const username = faker.internet.userName({firstName: firstname, lastName: lastna
 <!--  -->
 
 <template>
-
-<!--{{ fullname }} <br />  -->
-
-
-<div class="border border-gray-400 rounded-lg shadow bg-gray-200 h-full">
-    <img :src="faker.image.urlLoremFlickr({category: 'cat'})" />
-
-
-    <div class="p-4">
-        <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
-        <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
-        <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
-
+    <div class="border border-gray-500 rounded-lg shadow bg-gray-400 h-full">
+        <img class="object-fill h-480 w-960" v-bind:src="faker.image.urlLoremFlickr({category: 'cats', height: 480, width: 960})" />
+        <div class="p-4">
+            <p class="mb-2 text-2xl font-bold">{{ firstname }} {{ lastname }}</p>
+            <p class="mb-3 font-normal text-gray-800">{{ jobtitle }}</p>
+            <p class="mb-3 font-normal text-gray-800">{{ bio }}</p>
+        </div>
     </div>
-
-</div>
-
-
-
-
-
 </template>
-
-
 
 
 
