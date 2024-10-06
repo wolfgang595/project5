1feae862f0df4db0a477a08259f3618c76c7abe0
Nathan Serna <nath2534@outlook.com>
2024-10-06 06:11:38 -0500

    added nav guard

--- src/router.js
+++ src/router.js
@@ -1,22 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/views/HomePage.vue'
-import CardDetails from './views/CardDetails.vue'
-import LoginPage from './views/LoginPage.vue'
-import SettingsPage from './views/SettingsPage.vue'
+import CardDetails from '@/views/CardDetails.vue'
+import LoginPage from '@/views/LoginPage.vue'
+import SettingsPage from '@/views/SettingsPage.vue'
 
+import { useAuth } from './composables/useAuth'
+
+const {isAuthenticated} = useAuth()
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
-  { path: '/employees/:id', name: 'CardDetails', component: CardDetails },
-  { path: '/login', name: 'LoginPage', component: LoginPage },
-  { path: '/settings', name: 'SettingsPage', component: SettingsPage }
+  { path: '/homework5', name: 'Home', component: HomePage },
+  { path: '/homework5/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+
+  { path: '/homework5/employees/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/homework5/login', name: 'LoginPage', component: LoginPage },
+  { path: '/homework5/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+
+
+//nav guard
+router.beforeEach((to, _, next) => {
+  if (to.meta.requiresAuth && !isAuthenticated.value) {
+    next({name:'LoginPage', query:{redirect: to.fullPath}})
+  } else {
+    next()
+  }
+})
+
+
+
+
 
 export default router
