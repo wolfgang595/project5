b8400aa183e0c0e61e65c096df45a3d1783faf36
Nathan Serna <nath2534@outlook.com>
2024-10-06 19:28:11 -0500

    added instructions in settings page.

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -1,3 +1,24 @@
 <template>
-    Settings
+    
+
+
+
+
+
+
+
+
+    
+<div class="border border-blue-600 rounded-lg shadow bg-blue-600 h-96 ">
+        <div class="p-4 text-center text-slate-100 text-lg">
+        Welcome to setings page. Here you can modify your settings and customize how you view my website. For example, you can enable dark mode or even change colors to your liking.
+            <div>
+                Your settings will save to your account specifically and take affect once you login.
+            </div>
+
+
+        </div>
+    </div>
+
+    
 </template>
