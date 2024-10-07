9ffa476f224e6ed7b0909d4beec8ebd3eb4b15ba
Nathan Serna <nath2534@outlook.com>
2024-10-06 19:20:06 -0500

    Finished OtherPage.vue

--- src/views/OtherPage.vue
+++ src/views/OtherPage.vue
@@ -1,14 +1,20 @@
 <template>
 
-  <h1>Other</h1>
   
+  
+<div class="border border-blue-600 rounded-lg shadow bg-blue-600 h-96 ">
+        <div class="p-4 text-center text-slate-100 text-lg">
+            Hello! Welcome to my Other page. Here you can contact me and share your favorite photo of a plane. Best plane photo wins a cash prize of $100!!
 
-  <div>
+            <div>
+                    Email: admin@stc.com
+            </div>
+
+
+        </div>
+    </div>
 
     
-  </div>
-
-
 
 
 </template>
